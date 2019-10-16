d09d686f86c7763ec09c2e3e801de9e610058a9e
Ruizhi Wang <ruizhiwang@dhcp-10-248-98-252.eduroam.wireless.private.cam.ac.uk>
2019-10-16 18:53:21 +0100

    testing it branching worked

--- 20191016_main_v0.py
+++ 20191016_main_v0.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from class_setup import class_setup_main
 from class_mainwindow import class_mainwindow_main
 
+# Comment to test if git works
 
 # Define controller to act
 class Controller:
